da1878667e0f02ad9dbf7fdc54ea4debfb67ae73
Joel Delight <thefencingflutist@gmail.com>
2016-06-23 17:28:17 -0700

    add links for the songs that play in each room

--- js/soundlib.js
+++ js/soundlib.js
@@ -13,3 +13,16 @@ var swordSound = new Audio("audio/sword1.mp3");
 var chimes = new Audio("audio/chimes.mp3");
 
 var fireBallSound = new Audio("audio/fireball.mp3");
+
+var overworldMusic = new Audio("music/Michal_Hambourg_-_01_-_Chopin_Etude_in_A-flat_Op_25_no_1.mp3");
+overworldMusic.loop = true;
+
+var lightRoomMusic = new Audio("music/Samuil_Feinberg_-_01_-_Bach-Feinburg_Prelude_in_E.mp3");
+lightRoomMusic.loop = true;
+
+var boulderRoomMusic = new Audio("music/Ketsa_-_10_-_From_Memory.mp3");
+boulderRoomMusic.loop = true;
+
+var deathMusic = new Audio("music/Kai_Engel_-_08_-_Far_From_Home_PON_VII.mp3");
+var oniBattleMusic = new Audio("music/Jason_Weinberger.mp3");
+oniBattleMusic.loop = true;
